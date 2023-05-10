86f8d16dee1d9e4c13e9323e88d303673fae9db4
Ivan Zhukov <ivan.zhukov@student.griffith.ie>
2023-05-10 05:16:14 +0100

    test

--- app.js
+++ app.js
@@ -69,14 +69,20 @@ app.post(
   login,
 );
 
+// app.get('/api/getQuestions',(req,res) => {
+//   Question.find({})
+//     .then((questions) => {
+//       res.status(200).send(questions);
+//     })
+// });
+
+app.use(auth);
 app.get('/api/getQuestions',(req,res) => {
   Question.find({})
     .then((questions) => {
       res.status(200).send(questions);
     })
 });
-
-app.use(auth);
 
 app.use('/api/users', routesUsers);
 
